157dcb90b29995649d5e5eb41216ba76fe2b63b6
jonathan-stein <jonathanstein98@gmail.com>
2018-04-30 01:31:02 -0500

    gets full text of tweets

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -8,8 +8,8 @@ api = tweepy.API(auth)
 
 def get_user_tweets(username):
     f = open('tweets.txt', 'w')
-    for tweet in tweepy.Cursor(api.user_timeline,id=username).items():
-        f.write(tweet.text + '\n')
+    for tweet in tweepy.Cursor(api.user_timeline,id=username, tweet_mode='extended').items():
+        f.write(tweet.full_text + '\n')
     f.close()
 
 if __name__ == '__main__':
