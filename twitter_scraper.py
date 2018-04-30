a0908d2bd6445fe2b541cb18ba20110dc4884eae
jonathan-stein <jonathanstein98@gmail.com>
2018-04-30 00:53:57 -0500

    changed so that scraper wrote to file

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -7,8 +7,10 @@ auth.set_access_token(keys.access_token, keys.access_token_secret)
 api = tweepy.API(auth)
 
 def get_user_tweets(username):
+    f = open('tweets.txt', 'w')
     for tweet in tweepy.Cursor(api.user_timeline,id=username).items():
-        print(tweet.text)
+        f.write(tweet.text + '\n')
+    f.close()
 
 if __name__ == '__main__':
     get_user_tweets(sys.argv[1])
